3b4cb0602172eadd654e1a67b883733d18c42f9b
Tushar Grover <tushargrover222004@gmail.com>
2025-08-13 01:52:50 +0530

    feat: Update CORS configuration to allow all origins and improve request handling

--- server/index.js
+++ server/index.js
@@ -10,11 +10,14 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: 'https://image-drive.tushr.xyz', // You will replace this later
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+// const corsOptions = {
+//   origin: 'https://image-drive.tushr.xyz',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+
+app.use(cors({ origin: '*', methods: ['GET','POST','PUT','DELETE','OPTIONS'], allowedHeaders: ['Content-Type','Authorization'] }));
+app.options('*', cors());
 
 app.use(express.json());
 
